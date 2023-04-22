d764c4b3cf3537a470a15570d6e5f4ee234e22ce
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-22 13:04:54 -0300

    adding stylying to content

--- src/App.js
+++ src/App.js
@@ -12,7 +12,39 @@ function App() {
       <hr />
       <Hero />
       <hr />
-    <Content />
+        <div className="content-app">
+        <Content 
+            rating="5"
+            label="SOLD OUT"
+            image="katie"
+            quantity="6"
+            origin="USA"
+            service="Life lessons with Katie Zaferes"
+            pricing="$136"
+            // person : /person /couple etc...
+            person="/person" />
+            <Content
+            rating="5"
+            label="ONLINE"
+            image="wedding"
+            quantity="36"
+            origin="USA"
+            service="Learn wedding photography"
+            pricing="$125"
+            // person : /person /couple etc...
+            person="/person" />
+              <Content
+            rating="4.8"
+            label="ON SALE"
+            image="mountain"
+            quantity="2"
+            origin="USA"
+            service="Group mountain Biking"
+            pricing="$50"
+            // person : /person /couple etc...
+            person="/person" />
+        </div>
+   
     </div>
   
   );
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,36 @@
 import  '../styles/Content.css';
 import React from 'react';
-
+import { BsFillStarFill } from "react-icons/bs";
 
 class Content extends React.Component{
     render(){
         return(
-           <h1>Content</h1>
+            <div className='content-container'>
+        
+                <div className='div-content'>
+                        <div className='div-each-picture-plus-content'>
+                        <span className='label-img'>
+                            {this.props.label}
+                        </span>
+                        <img className='img-content' src={require(`../images/photo-${this.props.image}.png`)} alt={`require(${this.props.image})`} />
+                        <div className='star-quant-orig'>
+                            <div><BsFillStarFill className='star-icon' /> {this.props.rating}</div>
+                            <div>({this.props.quantity})</div>
+                            <div>-{this.props.origin}</div>
+                        </div>
+
+                        <p className='para-service'>{this.props.service}</p>
+
+                        <div className='div-pricing-person'>
+                            <p className='para-price'>From {this.props.pricing}</p>
+                            <p className='para-person'>{this.props.person}</p>
+                        </div>
+
+                        </div>
+                </div>
+           </div>
         )
+
     }
 }
 
