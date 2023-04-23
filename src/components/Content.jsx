dd391ec8304523044d793acf702678dac20c9b4a
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-23 19:54:47 -0300

    adding destructuration instead of props

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,26 +4,28 @@ import { BsFillStarFill } from "react-icons/bs";
 
 class Content extends React.Component{
     render(){
+        const {rating, label, image, quantity, origin, service, pricing, person } = this.props;
+
         return(
             <div className='content-container'>
         
                 <div className='div-content'>
                         <div className='div-each-picture-plus-content'>
-                        <span className='label-img'>
-                            {this.props.label}
+                        <span className='card-badge'>
+                            {label}
                         </span>
-                        <img className='img-content' src={require(`../images/photo-${this.props.image}.png`)} alt={`require(${this.props.image})`} />
+                        <img className='img-content' src={require(`../images/photo-${image}.png`)} alt={`require(${image})`} />
                         <div className='star-quant-orig'>
-                            <div><BsFillStarFill className='star-icon' /> {this.props.rating}</div>
-                            <div>({this.props.quantity})</div>
-                            <div>-{this.props.origin}</div>
+                            <div><BsFillStarFill className='star-icon' /> {rating}</div>
+                            <div>({quantity})</div>
+                            <div>-{origin}</div>
                         </div>
 
-                        <p className='para-service'>{this.props.service}</p>
+                        <p className='para-service'>{service}</p>
 
                         <div className='div-pricing-person'>
-                            <p className='para-price'>From {this.props.pricing}</p>
-                            <p className='para-person'>{this.props.person}</p>
+                            <p className='para-price'>From {pricing}</p>
+                            <p className='para-person'>{person}</p>
                         </div>
 
                         </div>
